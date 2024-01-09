57fb18137cea98ee04da246bc520b2a596c9cb05
navya <navya@metrictreelabs.com>
2024-01-09 23:29:35 +0530

    short url generator completed

--- server.js
+++ server.js
@@ -1,9 +1,38 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const shortUrlModel = require("./model/shortUrls");
+
+mongoose.connect(
+  "mongodb+srv://navya:MYpassw0rd@cluster0.flgzf6k.mongodb.net/urlShortner",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+);
+
+app.use(express.urlencoded({ extends: true }));
 app.set("view engine", "ejs");
-app.get("/", (req, res) => {
-  res.render("index");
+
+app.get("/", async (req, res) => {
+  let shorts = await shortUrlModel.find();
+  res.render("index", { shorts });
 });
-app.post("/short/urls", (req, res) => {});
+
+app.post("/short/urls", async (req, res) => {
+  await shortUrlModel.create({
+    full: req.body.fullUrl,
+  });
+  res.redirect("/");
+});
+
+app.get("/:shortUrl", async (req, res) => {
+  let url = await shortUrlModel.findOne({ short: req.params.shortUrl });
+  if (url == null) {
+    return res.sendStatus(404);
+  }
+  url.clicks++;
+  url.save();
+  res.redirect(url.full);
+});
 app.listen(process.env.PORT || 5000);
